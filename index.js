b50c94a5aefe232e7866bf18c1ff67c4a45c79cb
aekeeton <ashley.keeton@outlook.com>
2022-06-15 11:36:43 -0500

    commit tree f4b98954658e1f92fe2b7ed843ee4316f976a958

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const array = tutorials.map(function(tutorial) {
+    tutorial = tutorial.split(' ');
+    
+    for(let i = 0; i < tutorial.length; i ++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+      
+    }
+    return tutorial.join(' ');
+  })
+  return array;
 }
